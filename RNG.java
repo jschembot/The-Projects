67ea2002c29294df283954e3474de4fc3d5cc55b
emoon23 <emoon9323@gmail.com>
2016-08-24 02:58:58 -0500

    Add files via upload

---
+++ RNG.java
@@ -0,0 +1,13 @@
+import java.util.Random; //Generate random numbers with this library
+
+public class RNG {
+
+	public static void main(String[]args){
+		
+		Random rnd = new Random();
+		System.out.println(rnd.nextInt(2));
+		
+		
+	}
+	
+}
